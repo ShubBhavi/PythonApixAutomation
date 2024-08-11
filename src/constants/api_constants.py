884d0ac78e7437de45582101e646b124b770de1e
Shubham <Shubhambhavi888@gmail.com>
2024-08-11 14:46:33 +0530

    first commit

---
+++ src/constants/api_constants.py
@@ -0,0 +1,21 @@
+# adding constants
+
+class APIConstants(object):
+
+    @staticmethod
+    def base_url():
+        return "https://restful-booker.herokuapp.com"
+
+    @staticmethod
+    def create_booking():
+        return "https://restful-booker.herokuapp.com/booking"
+
+    @staticmethod
+    def create_token():
+        return "https://restful-booker.herokuapp.com/auth"
+
+
+# for put,patch,delete
+#     all of the testcases will be having booking id diff so we dont have static method
+    def put_patch_delete(self,booking_id):
+        return "https://restful-booker.herokuapp.com/booking/" + str(booking_id)
